61299473425a40ffe0a0c9f1e0c71475379d19b0
rpreda <rares.preda98@gmail.com>
2016-04-23 18:57:26 +0300

    Rover

---
+++ Assets/Scripts/MoveScript2.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript2 : MonoBehaviour {
+    float thrust = 100f;
+    public Rigidbody rb;
+    public Vector3 cameraFollowOffset = new Vector3(0, 10, -10);
+    public Camera player;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveScript player_movement = player.GetComponent<moveScript>();
+        if (Input.GetKeyDown("f") && Vector3.Distance(player.transform.position, transform.position) < 10)
+        {
+            if (player_movement.in_rover)
+            {
+                player_movement.in_rover = false;
+                player.transform.rotation.Set(0f, 0.3f, 0f, 0.9f);
+            }
+            else
+            {
+                Debug.Log(player.transform.rotation.ToString());
+                player_movement.in_rover = true;
+                player.transform.rotation = this.transform.rotation;
+            }
+            //rb.AddForce(new Vector3(thrust, 0, thrust));
+        }
+        if (player_movement.in_rover)
+        {
+            Vector3 rotation = new Vector3(0, Input.GetAxis("Horizontal") * 5, 0);
+            this.transform.Rotate(rotation);
+            float vert = Input.GetAxis("Vertical");
+            rb.AddForce(transform.forward * vert * thrust);
+            player.transform.position = transform.position + cameraFollowOffset;
+            player.transform.rotation = this.transform.rotation;
+        }
+	}
+}
--- Assets/Scripts/moveScript.cs
+++ Assets/Scripts/moveScript.cs
@@ -5,32 +5,36 @@ public class moveScript : MonoBehaviour {
     public float speed = 8.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 200F;
+    public bool in_rover = false;
     Vector3 rotation;
     private Vector3 moveDirection = Vector3.zero;
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
+        if (!in_rover)
         {
-            float dir = 0;
-            if (Input.GetKey("q"))
-                dir = -1;
-            if (Input.GetKey("e"))
-                dir = 1;
-            moveDirection = new Vector3(dir , 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            if (Input.GetKey("a") || Input.GetKey("d"))
-                rotation = new Vector3(0, Input.GetAxis("Horizontal") * 5, 0);
-            else
-                rotation = new Vector3(0, 0, 0);
+            CharacterController controller = GetComponent<CharacterController>();
+            if (controller.isGrounded)
+            {
+                float dir = 0;
+                if (Input.GetKey("q"))
+                    dir = -1;
+                if (Input.GetKey("e"))
+                    dir = 1;
+                moveDirection = new Vector3(dir, 0, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+                if (Input.GetKey("a") || Input.GetKey("d"))
+                    rotation = new Vector3(0, Input.GetAxis("Horizontal") * 5, 0);
+                else
+                    rotation = new Vector3(0, 0, 0);
 
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
+                if (Input.GetButton("Jump"))
+                    moveDirection.y = jumpSpeed;
 
+            }
+            controller.transform.Rotate(rotation);
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
         }
-        controller.transform.Rotate(rotation);
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
     }
 }
