84061b788e05a0e712a28b90c684663954544885
Dan Avram <Dan Avram>
2016-04-23 13:41:55 +0300

    + TubeSection, Rover, Sera prefabs + Required Materials

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resource {
+
+    public enum ResourceType { OXYGEN, FOOD, WATER, ENERGY };
+
+    ResourceType type;
+    int val;
+
+    public Resource (ResourceType type, int val)
+    {
+        this.type = type;
+        this.val = val;
+    }
+
+    public int GetVal()
+    {
+        return val;
+    }
+
+    public void AddToVal(int val)
+    {
+        this.val += val;
+    }
+
+    public void SetVal(int val)
+    {
+        this.val = val;
+    }
+}
