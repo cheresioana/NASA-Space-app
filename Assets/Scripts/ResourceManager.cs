0b79f012761bb1e00eb0e469e33aaf38addb814b
rpreda <rares.preda98@gmail.com>
2016-04-23 15:15:47 +0300

    ALL

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ResourceManager : MonoBehaviour {
+
+
+    enum ResourceType { OXYGEN, FOOD, WATER, ENERGY};
+
+    Dictionary<ResourceType, int> depletionRates;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
